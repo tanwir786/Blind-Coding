6ba13843f4c5945a4b396388f44f944a2e927b8f
Tanwir Ahmad <tanwirahmad2912@gmail.com>
2022-10-19 02:46:33 +0530

    initial commit

---
+++ userModel.js
@@ -0,0 +1,34 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+const opts = { toJSON: { virtuals: true } };
+const userSchema = new Schema({
+    name: {
+        type: String,
+        required: true,
+    },
+    admissionNumber: {
+        type: String,
+        required: true,
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    year: {
+        type: String,
+        required: true
+    },
+    time:{
+        type: Date,
+        required: true
+    },
+    code: {
+        type: String
+    },
+    disqualified:{
+        type: Boolean,
+    }
+
+}, opts);
+module.exports=mongoose.model('User', userSchema);
